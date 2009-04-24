b07cd6ab9c46ee1cf17658ccd61b1c0f8c599a7a
Stephen Celis <stephen@stephencelis.com>
2009-04-24 14:23:30 -0500

    Refactor heavily.

--- lib/ghi/cli.rb
+++ lib/ghi/cli.rb
@@ -4,226 +4,273 @@ require "ghi"
 require "ghi/api"
 require "ghi/issue"
 
-class GHI::CLI
-  attr_reader :user, :repo, :api, :action, :state, :number, :title,
-    :search_term
-
-  def initialize
-    option_parser.parse!(ARGV)
-
-    `git config --get remote.origin.url`.match %r{([^:/]+)/([^/]+).git$}
-    @user ||= $1
-    @repo ||= $2
-    @api = GHI::API.new user, repo
-
-    case action
-      when :search then search search_term, state
-      when :list   then list state
-      when :show   then show number
-      when :open   then open title
-      when :edit   then edit number
-      when :close  then close number
-      when :reopen then reopen number
-      else puts option_parser
+module GHI::CLI #:nodoc:
+  module FileHelper
+    def launch_editor(file)
+      system "#{editor} #{file.path}"
     end
-  rescue GHI::API::InvalidConnection
-    warn "#{File.basename $0}: not a GitHub repo"
-  rescue GHI::API::ResponseError => e
-    warn "#{File.basename $0}: #{e.message} (#{user}/#{repo})"
-  rescue OptionParser::InvalidOption, OptionParser::MissingArgument => e
-    warn "#{File.basename $0}: #{e.message}"
-  end
 
-  private
-
-  def option_parser
-    @option_parser ||= OptionParser.new { |opts|
-      opts.banner = "Usage: #{File.basename $0} [options]"
-
-      opts.on("-l", "--list", "--search", "--show [state|term|number]") do |v|
-        @action = :list
-        case v
-        when nil, /^o$/
-          @state = :open
-        when /^\d+$/
-          @action = :show
-          @number = v.to_i
-        when /^c$/
-          @state = :closed
-        else
-          @action = :search
-          @state ||= :open
-          @search_term = v
-        end
-      end
-
-      opts.on("-o", "--open", "--reopen [number]") do |v|
-        @action = :open
-        case v
-        when /^\d+$/
-          @action = :reopen
-          @number = v.to_i
-        when /^l$/
-          @action = :list
-          @state = :open
-        else
-          @title = v
-        end
-      end
-
-      opts.on("-c", "--closed", "--close [number]") do |v|
-        case v
-        when /^\d+$/
-          @action = :close
-          @number = v.to_i
-        when /^l/, nil
-          @action = :list
-          @state = :closed
-        else
-          raise OptionParser::InvalidOption
-        end
-      end
-
-      opts.on("-e", "--edit [number]") do |v|
-        case v
-        when /^\d+$/
-          @action = :edit
-          @state = :closed
-          @number = v.to_i
-        else
-          raise OptionParser::MissingArgument
-        end
-      end
-
-      opts.on("-r", "--repo", "--repository [name]") do |v|
-        repo = v.split "/"
-        repo.unshift GHI.login if repo.length == 1
-        @user, @repo = repo
-      end
-
-      opts.on("-V", "--version") do
-        puts "#{File.basename($0)}: v#{GHI::VERSION}"
-        exit
-      end
-
-      opts.on("-h", "--help") do
-        puts opts
-        exit
-      end
-    }
-  end
-
-  def search(term, state)
-    issues = api.search term, state
-    puts "# #{state.to_s.capitalize} #{term.inspect} issues on #{user}/#{repo}"
-    if issues.empty?
-      puts "none"
-    else
-      puts issues.map { |i| "  #{i.number.to_s.rjust(3)}: #{i.title[0,72]}" }
-    end
-  end
-
-  def list(state)
-    issues = api.list state
-    puts "# #{state.to_s.capitalize} issues on #{user}/#{repo}"
-    if issues.empty?
-      puts "none"
-    else
-      puts issues.map { |i| "  #{i.number.to_s.rjust(3)}: #{i.title[0,72]}" }
-    end
-  end
-
-  def show(number)
-    issue = api.show number
-    puts <<-BODY
-  #{issue.number}: #{issue.title} [#{issue.state}]
-
-       votes:  #{issue.votes}
-  created_at:  #{issue.created_at}
-  updated_at:  #{issue.updated_at}
-
-  #{issue.body}
-
-  -- #{issue.user}
-BODY
-  end
-
-  def open(title)
-    edit = ENV["VISUAL"] || ENV["EDITOR"] || "vi"
-    temp = Tempfile.open("open-issue-")
-    temp.write edit_message(GHI::Issue.new(:title => title))
-    temp.rewind
-    system "#{edit} #{temp.path}"
-    lines = File.readlines(temp.path).find_all { |l| !l.match(/^#/) }
-    temp.close!
-    if lines.to_s =~ /\A\s*\Z/
-      warn "can't file empty issue"
-      exit 1
-    else
-      title = lines.shift.strip
-      body = lines.join.sub(/\b\n\b/, " ").strip
-      issue = api.open title, body
-      puts "  Opened issue #{issue.number}: #{issue.title[0,58]}"
-    end
-  end
-
-  def edit(number)
-    edit = ENV["VISUAL"] || ENV["EDITOR"] || "vi"
-    temp = Tempfile.open("open-issue-")
-    issue = api.show number
-    temp.write edit_message(issue)
-    temp.rewind
-    system "#{edit} #{temp.path}"
-    lines = File.readlines(temp.path)
-    if temp.readlines == lines
-      warn "no change"
-      exit 1
-    else
-      lines.reject! { |l| l.match(/^#/) }
-      if lines.to_s =~ /\A\s*\Z/
-        warn "can't file empty issue"
-        exit 1
+    def gets_from_editor(issue)
+      if gitdir
+        File.open message_path, "a+", &file_proc(issue)
       else
-        title = lines.shift.strip
-        body = lines.join.sub(/\b\n\b/, " ").strip
-        issue = api.edit number, title, body
-        puts "  Updated issue #{issue.number}: #{issue.title[0,58]}"
+        Tempfile.new message_filename, &file_proc(issue)
+      end
+      return @message.shift.strip, @message.join.sub(/\b\n\b/, " ").strip
+    end
+
+    def delete_message
+      File.delete message_path
+    rescue TypeError
+      nil
+    end
+
+    def message_path
+      File.join gitdir, message_filename
+    end
+
+    private
+
+    def editor
+      ENV["VISUAL"] || ENV["EDITOR"] || "vi"
+    end
+
+    def gitdir
+      @gitdir ||= begin
+        dirs = []
+        Dir.pwd.count("/").times { |n| dirs << ([".."] * n << ".git") * "/" }
+        Dir[*dirs].first
       end
     end
-  ensure
-    temp.close!
+
+    def message_filename
+      @message_filename ||= "GHI_#{action.to_s.upcase}_MESSAGE"
+    end
+
+    def file_proc(issue)
+      lambda do |file|
+        file << edit_format(issue).join("\n") if File.zero? message_path
+        file.rewind
+        launch_editor file
+        lines = File.readlines file.path
+        @message = lines.find_all { |l| !l.match(/^#/) }
+        raise "can't file empty issue" if message.to_s =~ /\A\s*\Z/
+        raise "no change"              if file.readlines == lines
+      end
+    end
   end
 
-  def close(number)
-    issue = api.close number
-    puts "  Closed issue #{issue.number}: #{issue.title[0,58]}"
+  module FormattingHelper
+    def list_format(issues, term = nil)
+      l = if term
+        ["# #{state.to_s.capitalize} #{term.inspect} issues on #{user}/#{repo}"]
+      else
+        ["# #{state.to_s.capitalize} issues on #{user}/#{repo}"]
+      end
+
+      l += unless issues.empty?
+        issues.map { |i| "  #{i.number.to_s.rjust 3}: #{truncate i.title, 72}" }
+      else
+        ["none"]
+      end
+    end
+
+    def edit_format(issue)
+      l = []
+      l << issue.title                          if issue.title
+      l << ""
+      l << issue.body                           if issue.body
+      l << "# Please explain the issue. The first line will become the title."
+      l << "# Lines beginning '#' will be ignored. Empty issues won't be filed."
+      l << "# All line breaks will be honored in accordance with GFM:"
+      l << "#"
+      l << "#   http://github.github.com/github-flavored-markdown"
+      l << "#"
+      l << "# On #{user}/#{repo}:"
+      l << "#"
+      l += show_format(issue, false).map { |line| "# #{line}" }
+    end
+
+    def show_format(issue, verbose = true)
+      l = []
+      l << "      number:  #{issue.number}"     if issue.number
+      l << "       state:  #{issue.state}"      if issue.state
+      l << "       title:  #{issue.title}"      if issue.title
+      l << "        user:  #{issue.user || GHI.login}"
+      l << "       votes:  #{issue.votes}"      if issue.votes
+      l << "  created at:  #{issue.created_at}" if issue.created_at
+      l << "  updated at:  #{issue.updated_at}" if issue.updated_at
+      return l unless verbose
+      l << ""
+      l += issue.body.scan(/.{0,75}(?:\s|$)/).map { |line| "    #{line}" }
+    end
+
+    def action_format(issue)
+      key = "#{action.to_s.capitalize.sub(/e?$/, "ed")} issue #{issue.number}"
+      "#{key}: #{truncate issue.title, 78 - key.length}"
+    end
+
+    def truncate(string, length)
+      result = string.scan(/.{0,#{length}}(?:\s|$)/).first.strip
+      result << "..." if result != string
+      result
+    end
   end
 
-  def reopen(number)
-    issue = api.reopen number
-    puts "  Reopened issue #{issue.number}: #{issue.title[0,56]}"
-  end
+  class Executable
+    include FileHelper, FormattingHelper
 
-  def edit_message(issue)
-    l = []
-    l << issue.title.to_s                       if issue.title
-    l << ""
-    l << issue.body.to_s                        if issue.body
-    l << "# Please explain the issue. The first line will become the title."
-    l << "# Lines beginning '#' will be ignored. Empty issues won't be filed."
-    l << "# All line breaks will be honored in accordance with GFM:"
-    l << "#"
-    l << "#   http://github.github.com/github-flavored-markdown"
-    l << "#"
-    l << "# On #@user/#@repo:"
-    l << "#"
-    l << "#       number:  #{issue.number}"     if issue.number
-    l << "#        state:  #{issue.state}"      if issue.state
-    l << "#        title:  #{issue.title}"      if issue.title
-    l << "#         user:  #{issue.user || GHI.login}"
-    l << "#        votes:  #{issue.votes}"      if issue.votes
-    l << "#   created at:  #{issue.created_at}" if issue.created_at
-    l << "#   updated at:  #{issue.updated_at}" if issue.updated_at
-    l.join "\n"
+    attr_reader :message, :user, :repo, :api, :action, :state, :number, :title,
+      :search_term
+
+    def initialize
+      option_parser.parse!(ARGV)
+
+      `git config --get remote.origin.url`.match %r{([^:/]+)/([^/]+).git$}
+      @user ||= $1
+      @repo ||= $2
+      @api = GHI::API.new user, repo
+
+      case action
+        when :search then search search_term, state
+        when :list   then list state
+        when :show   then show number
+        when :open   then open title
+        when :edit   then edit number
+        when :close  then close number
+        when :reopen then reopen number
+        else puts option_parser
+      end
+    rescue GHI::API::InvalidConnection
+      warn "#{File.basename $0}: not a GitHub repo"
+      exit 1
+    rescue GHI::API::ResponseError => e
+      warn "#{File.basename $0}: #{e.message} (#{user}/#{repo})"
+      exit 1
+    rescue OptionParser::InvalidOption, OptionParser::MissingArgument => e
+      warn "#{File.basename $0}: #{e.message}"
+      exit 1
+    rescue StandardError => e
+      warn e.message
+      exit 1
+    end
+
+    private
+
+    def option_parser
+      @option_parser ||= OptionParser.new { |opts|
+        opts.banner = "Usage: #{File.basename $0} [options]"
+
+        opts.on("-l", "--list", "--search", "--show [state|term|number]") do |v|
+          @action = :list
+          case v
+          when nil, /^o$/
+            @state = :open
+          when /^\d+$/
+            @action = :show
+            @number = v.to_i
+          when /^c$/
+            @state = :closed
+          else
+            @action = :search
+            @state ||= :open
+            @search_term = v
+          end
+        end
+
+        opts.on("-o", "--open", "--reopen [number]") do |v|
+          @action = :open
+          case v
+          when /^\d+$/
+            @action = :reopen
+            @number = v.to_i
+          when /^l$/
+            @action = :list
+            @state = :open
+          else
+            @title = v
+          end
+        end
+
+        opts.on("-c", "--closed", "--close [number]") do |v|
+          case v
+          when /^\d+$/
+            @action = :close
+            @number = v.to_i
+          when /^l/, nil
+            @action = :list
+            @state = :closed
+          else
+            raise OptionParser::InvalidOption
+          end
+        end
+
+        opts.on("-e", "--edit [number]") do |v|
+          case v
+          when /^\d+$/
+            @action = :edit
+            @state = :closed
+            @number = v.to_i
+          else
+            raise OptionParser::MissingArgument
+          end
+        end
+
+        opts.on("-r", "--repo", "--repository [name]") do |v|
+          repo = v.split "/"
+          repo.unshift GHI.login if repo.length == 1
+          @user, @repo = repo
+        end
+
+        opts.on("-V", "--version") do
+          puts "#{File.basename($0)}: v#{GHI::VERSION}"
+          exit
+        end
+
+        opts.on("-h", "--help") do
+          puts opts
+          exit
+        end
+      }
+    end
+
+    def search(term, state)
+      issues = api.search term, state
+      puts list_format(issues, term)
+    end
+
+    def list(state)
+      issues = api.list state
+      puts list_format(issues)
+    end
+
+    def show(number)
+      issue = api.show number
+      puts show_format(issue)
+    end
+
+    def open(title)
+      title, body = gets_from_editor GHI::Issue.new(:title => title)
+      issue = api.open title, body
+      delete_message
+      puts action_format(issue)
+    end
+
+    def edit(number)
+      title, body = gets_from_editor api.show(number)
+      issue = api.edit number, title, body
+      delete_message
+      puts action_format(issue)
+    end
+
+    def close(number)
+      issue = api.close number
+      puts action_format(issue)
+    end
+
+    def reopen(number)
+      issue = api.reopen number
+      puts action_format(issue)
+    end
   end
 end
