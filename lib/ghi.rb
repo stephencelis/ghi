1569e4a8d12705a37c8ca627074625d457afcf6d
Jamie Macey <jamie.git@tracefunc.com>
2009-04-24 22:53:21 -0400

    Prompt user if they haven't set up github in .gitconfig

--- lib/ghi.rb
+++ lib/ghi.rb
@@ -2,10 +2,24 @@ module GHI
   VERSION = "0.0.4"
 
   def self.login
-    `git config --get github.user`.chomp
+    return @login if defined? @login
+    @login = `git config --get github.user`.chomp
+    if @login.empty?
+      print "Please enter your GitHub username: "
+      @login = gets
+      `git config --global github.user #@login`
+    end
+    @login
   end
 
   def self.token
-    `git config --get github.token`.chomp
+    return @token if defined? @token
+    @token = `git config --get github.token`.chomp
+    if @token.empty?
+      print "GitHub token (https://github.com/account): "
+      @token = gets
+      `git config --global github.token #@token`
+    end
+    @token
   end
 end
