068d16cfe32938185ecbd032afac9b6e920de5d9
Patrick Collison <patrick@collison.ie>
2011-10-21 11:53:32 -0700

    Match remotes that don't end in .git

--- lib/ghi/cli.rb
+++ lib/ghi/cli.rb
@@ -233,7 +233,7 @@ module GHI::CLI #:nodoc:
       @args, @argv = argv, argv.dup
 
       remotes = `git config --get-regexp remote\..+\.url`.split /\n/
-      repo_expression = %r{([^:/]+)/([^/\s]+)(?:\.git)$}
+      repo_expression = %r{([^:/]+)/([^/\s]+)(?:\.git)?$}
       if remote = remotes.find { |r| r.include? "github.com" }
         remote.match repo_expression
         @user, @repo = $1, $2
