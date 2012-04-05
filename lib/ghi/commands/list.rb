ffa599aa1f5c4c1b5b6ed01406fd47a84bc4ec2c
Stephen Celis <stephen@stephencelis.com>
2012-04-04 18:43:59 -0700

    Avoid empty space.

--- lib/ghi/commands/list.rb
+++ lib/ghi/commands/list.rb
@@ -103,7 +103,7 @@ module GHI
         res = throb(
           0, format_state(assigns[:state], quiet ? CURSOR[:up][1] : '#')
         ) { api.get uri, assigns }
-        page "\r#{CURSOR[:up][1]}#{header}" do
+        page header && "\r#{CURSOR[:up][1]}#{header}" do
           issues = res.body
           if verbose
             puts issues.map { |i| format_issue i }
