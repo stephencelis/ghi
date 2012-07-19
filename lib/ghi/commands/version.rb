9a549e5aaadc55deba32862e27e503782d952c87
Stephen Celis <stephen@stephencelis.com>
2012-07-19 12:02:35 -0700

    Bump version
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- lib/ghi/commands/version.rb
+++ lib/ghi/commands/version.rb
@@ -4,7 +4,7 @@ module GHI
       MAJOR   = 0
       MINOR   = 9
       PATCH   = 0
-      PRE     = 20120627
+      PRE     = 20120719
 
       VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join '.'
 
