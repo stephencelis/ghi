stephencelis/ghi

commit f6c6315ff940b9853b95aef72e27035275966c7e

Fix issue #88

lib/ghi/commands/command.rb
@@ -37,7 +37,7 @@ module GHI
 
       def repo
         return @repo if defined? @repo
-        @repo = GHI.config('ghi.repo') || detect_repo
+        @repo = detect_repo || GHI.config('ghi.repo')
         if @repo && !@repo.include?('/')
           @repo = [Authorization.username, @repo].join '/'
         end

commit 19033a00a62e6f8499eb0289815548f2d0c1a8f0

Fix ghi.repo override
This stopped working with a regression introduced in f6c6315f. The
previous behavior, however, failed to allow for normal global behavior.

Fixes #137.

Signed-off-by: Stephen Celis <stephen@stephencelis.com>

lib/ghi.rb
@@ -109,9 +109,12 @@ EOF
       exit 1
     end
 
-    def config key, upcase = true
+    def config key, options = {}
+      upcase = options.fetch :upcase, true
+      flags = options[:flags]
       var = key.gsub('core', 'git').gsub '.', '_'
-      value = ENV[upcase ? var.upcase : var] || `git config #{key}`.chomp
+      var.upcase! if upcase
+      value = ENV[var] || `git config #{flags} #{key}`.chomp
       value unless value.empty?
     end
 

lib/ghi/client.rb
@@ -114,8 +114,8 @@ module GHI
       end
       req.basic_auth username, password if username && password
 
-      proxy   = GHI.config 'https.proxy', false
-      proxy ||= GHI.config 'http.proxy',  false
+      proxy   = GHI.config 'https.proxy', :upcase => false
+      proxy ||= GHI.config 'http.proxy',  :upcase => false
       if proxy
         proxy = URI.parse proxy
         http = Net::HTTP::Proxy(proxy.host, proxy.port).new HOST, PORT

lib/ghi/commands/command.rb
@@ -37,7 +37,7 @@ module GHI
 
       def repo
         return @repo if defined? @repo
-        @repo = detect_repo || GHI.config('ghi.repo')
+        @repo = GHI.config('ghi.repo', :flags => '--local') || detect_repo
         if @repo && !@repo.include?('/')
           @repo = [Authorization.username, @repo].join '/'
         end