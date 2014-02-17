6d3c5420228373e3403f13cc84d1e9ee1ee08d27
LFDM <1986gh@gmail.com>
2014-02-17 16:41:48 +0100

    First rough implementation of a find command

---
+++ lib/ghi/commands/find.rb
@@ -0,0 +1,75 @@
+module GHI
+  module Commands
+    class Find < List
+      def options
+        OptionParser.new do |opts|
+          opts.banner = 'usage: ghi find [options] <keywords>'
+          opts.separator ''
+          parse_globality(opts)
+        end
+      end
+
+      def execute
+        begin
+          extract_keywords
+          options.parse!(args)
+        rescue OptionParser::InvalidOption => e
+          fallback.parse! e.args
+          retry
+        end
+
+        assigns[:repo] ||= repo if repo
+        morph_params_to_qualifers
+
+
+        unless quiet
+          print header = format_issues_header
+          print "\n" unless paginate?
+        end
+
+        res = throb(
+          0, format_state(assigns[:state], quiet ? CURSOR[:up][1] : '#')
+        ) { api.get uri, assigns }
+
+        print "\r#{CURSOR[:up][1]}" if header && paginate?
+
+        page header do
+          issues = res.body['items']
+          puts format_issues(issues, repo.nil?)
+
+          break unless res.next_page
+          res = throb { api.get res.next_page }
+        end
+      end
+
+      private
+
+      def extract_keywords
+        puts args
+        keywords = []
+        keywords << args.shift until args.empty? || args.first.start_with?('-')
+        # raise missing_keywords if keywords.empty? || args.empty?
+
+        assigns[:q] = keywords.join(' ')
+      end
+
+      def morph_params_to_qualifers
+        params = [:q, :sort, :order]
+        # this should be safe to do, because :q is always the first element in assigns
+        copy = assigns.dup
+        assigns.clear
+        copy.each do |k, v|
+          if params.include?(k)
+            assigns[k] = v
+          else
+            assigns[:q] << " #{k}:#{v}"
+          end
+        end
+      end
+
+      def uri
+        "/search/issues"
+      end
+    end
+  end
+end
