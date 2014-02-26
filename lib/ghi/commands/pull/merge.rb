8a8bc7baf2cf0eeae755f4d4f1ce3ee1bd2271c6
LFDM <1986gh@gmail.com>
2014-02-26 17:03:33 +0100

    Handle errors and warning in Pull::Merge

--- lib/ghi/commands/pull/merge.rb
+++ lib/ghi/commands/pull/merge.rb
@@ -20,6 +20,10 @@ module GHI
       def execute
         subcommand_execute
 
+        abort already_merged if merged?
+        abort dirty_pull_request unless mergeable?
+        ask_for_continuation if needs_rebase?
+
         begin
           merge_pull_request
         rescue
@@ -29,8 +33,34 @@ module GHI
 
       private
 
+      def ask_for_continuation
+        print rebase_warning
+        abort "\nThanks! Your commit history is grateful." unless $stdin.gets.chomp == 'Y'
+      end
+
+      def rebase_warning
+        [
+          "\b" + fg('e1811d') { "#{head} and #{base} have diverged!" },
+          '',
+          "To retain a clean commit history it is recommended to rebase before merging.",
+          'Do you really want to do this? (type Y to continue) '
+        ].join("\n")
+      end
+
+      def already_merged_message
+        "Pull request has already been merged.\n" + more_info
+      end
+
+      def dirty_pull_request
+        "Cannot merge a dirty pull request.\n" + more_info
+      end
+
+      def more_info
+        "See 'ghi pull show #{issue}' for further information."
+      end
+
       def commit_message
-        @commit_message
+        @commit_message ||= pr['title']
       end
 
       def merge_pull_request
@@ -39,6 +69,14 @@ module GHI
 
       def merge_uri
         "#{pull_uri}/merge"
+      end
+
+      def merged?
+        pr['merged']
+      end
+
+      def mergeable?
+        pr['mergeable']
       end
     end
   end
