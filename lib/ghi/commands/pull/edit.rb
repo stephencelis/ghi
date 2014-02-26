09436ab4e2f58f719a56d16d09df82d7a468c940
LFDM <1986gh@gmail.com>
2014-02-27 00:44:34 +0100

    Implements editing of pull requests in Pull::Edit

--- lib/ghi/commands/pull/edit.rb
+++ lib/ghi/commands/pull/edit.rb
@@ -2,9 +2,69 @@ module GHI
   module Commands
     class Pull::Edit < Pull
       def options
+        OptionParser.new do |opts|
+          opts.banner = "edit - edits the title and body of your pull request"
+          opts.on('-s', '--show', 'show the PR after successful edition') { @show = true }
+        end
       end
 
       def execute
+        subcommand_execute
+
+        editor.start(template)
+        editor.require_content_for(:title, :body)
+        editor.check_for_changes(title: title, body: body)
+        editor.unlink
+
+        begin
+          edit_pull_request
+          puts "Pull request successfully edited."
+          show_edited_pull_request if @show
+        rescue
+          # TODO
+        end
+      end
+
+      private
+
+      def edit_pull_request
+        throb { api.patch(pull_uri, editor.content) }
+      end
+
+      def show_edited_pull_request
+        exec "ghi pull show #{issue}"
+      end
+
+      def title
+        pr['title']
+      end
+
+      def body
+        pr['body']
+      end
+
+      def editor
+        @editor ||= Editor.new('GHI_PULL_REQUEST_EDIT')
+      end
+
+      def template
+<<EOF
+@ghi-title@ #{title}
+
+#{body}
+#{template_explanation}
+EOF
+      end
+
+      def template_explanation
+<<EOF
+# Edit you pull request. You can edit the title, but please don't touch
+# the keyword itself.  Trailing lines starting with '#' (like these)
+# will be ignored, and empty messages will not be submitted. Issues are
+# formatted with GitHub Flavored Markdown (GFM):
+#
+#   http://github.github.com/github-flavored-markdown
+EOF
       end
     end
   end
