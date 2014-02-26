68b14933966ea3d2628376e1a1fd29798ae75933
LFDM <1986gh@gmail.com>
2014-02-26 23:48:38 +0100

    Add Pull::Close

--- lib/ghi/commands/pull/close.rb
+++ lib/ghi/commands/pull/close.rb
@@ -1,10 +1,29 @@
 module GHI
   module Commands
-    class Pull::Close < Pull
+    class Pull::Close < Pull::Show
       def options
+        "close - closes a pull request without merging"
       end
 
       def execute
+        handle_help_request
+        require_issue
+        extract_issue
+
+        begin
+          @pr = close_pull_request
+          honor_the_issue_contract
+          show_pull_request
+          puts 'Unmerged pull request closed.'
+        rescue
+          # TODO
+        end
+      end
+
+      private
+
+      def close_pull_request
+        throb { api.patch(pull_uri, state: 'closed') }.body
       end
     end
   end
