0c8fc6fd7549dda6c135c5cd018eb947105f5e35
LFDM <1986gh@gmail.com>
2014-02-26 22:57:16 +0100

    Add new interface for Editor
    Will be used by pull, other refactorings are open for debate.

--- lib/ghi/editor.rb
+++ lib/ghi/editor.rb
@@ -44,5 +44,68 @@ module GHI
       dir = `git rev-parse --git-dir 2>/dev/null`.chomp
       dir unless dir.empty?
     end
+
+    # Possibly new editor interface
+    #
+    # Currently only used by the pull command, but could be the base
+    # for a bigger refactoring. Adds flexibility.
+    public
+
+    def start(template)
+      File.open path, 'a+' do |f|
+        f << template if File.zero? path
+        f.rewind
+        system "#{editor} #{f.path}"
+        parse(f.path)
+      end
+    end
+
+    # TODO
+    # allow a hash here as well:
+    #   key: what's required
+    #   val: error message as string or proc
+    def require_content_for(*contents)
+      contents.each do |c|
+        unless content[c] && ! content[c].empty?
+          abort "#{c.capitalize} must not be empty!"
+        end
+      end
+    end
+
+    def check_uniqueness(a, b)
+      x, y = content.values_at(a, b)
+      abort "#{a} must not be the same as #{b}" if x == y
+    end
+
+    def content
+      @content ||= {}
+    end
+
+    private
+
+    def parse(file)
+      txt = File.read(file)
+      strip_explanation_lines(txt)
+      extract_keywords(txt, :title, :head, :base)
+      content[:body] = txt.strip
+    end
+
+    def extract_keywords(txt, *keywords)
+      keywords.each do |kw|
+        extract_keyword(txt, kw)
+      end
+    end
+
+    def extract_keyword(txt, kw, array = false)
+      txt.sub!(/^@ghi-#{kw}@(.*)(\n|$)/, '')
+      val = $1.to_s.strip
+      val = val.split(', ') if array
+      content[kw] = val
+    end
+
+    def strip_explanation_lines(txt)
+      txt.gsub!(/(?:^#.*$\n?)+\s*\z/, '')
+      txt.strip!
+    end
   end
 end
