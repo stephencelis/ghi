63b4f4e904f4388408986350365fd1abdae8c25c
LFDM <1986gh@gmail.com>
2014-02-27 20:07:07 +0100

    First implementation of diffing with comments

--- lib/ghi/commands/pull/show.rb
+++ lib/ghi/commands/pull/show.rb
@@ -6,8 +6,9 @@ module GHI
           opts.banner = "show - displays details of a pull request"
           opts.on('-c', '--commits', 'show associated commits') { commits; abort }
           opts.on('-f', '--files', 'show changed files') { files; abort }
-          opts.on('-d', '--diff', 'show diff') { diff; abort }
           opts.on('-p', '--patch', 'show patch') { patch; abort}
+          opts.on('-d', '--diff', 'show diff with review comments') { diff_with_comments; abort }
+          opts.on('-D', '--no-comment-diff', 'show diff without review comments') { diff; abort }
         end
       end
 
@@ -32,6 +33,52 @@ module GHI
         output_from_html(:patch)
       end
 
+      def diff_with_comments
+        # make sure the lazy evaluation of repo has been run before -
+        # we'll get an ugly race condition otherwise
+        require_repo
+        diff_call = lambda { throb { get_html web_uri('diff') } }
+        comm_call = lambda { api.get("#{pull_uri}/comments").body }
+        diff, comments = do_threaded(diff_call, comm_call)
+        grouped_comment_ids = group_by_diff_hunk(comments)
+        insert_place_holders(diff, grouped_comment_ids)
+        formatted = format_diff(diff)
+        replace_placeholders(formatted, comments)
+        puts formatted
+      end
+
+      def group_by_diff_hunk(comments)
+        comments.each_with_object(hash_with_default_array) do |comment, h|
+          h[comment['diff_hunk']] << comment['id']
+        end
+      end
+
+      def insert_place_holders(diff, comments)
+        # insert from the bottom up, so we don't destroy the indices
+        comments.keys.reverse.each do |hunk|
+          index = diff.index(hunk) + hunk.size
+          ids = comments[hunk]
+          placeholders = ids.map { |id| "\n" + placeholder(id) }.join
+          diff.insert(index, "\n" + placeholders)
+        end
+      end
+
+      def placeholder(id)
+        "@ghi-comment-#{id}@"
+      end
+
+      def replace_placeholders(diff, comments)
+        comments.each do |comment|
+          ph = placeholder(comment['id'])
+          c  = format_comment(comment).strip
+          diff.sub!(ph, "#{c}\n")
+        end
+      end
+
+      def hash_with_default_array
+        Hash.new { |h, k| h[k] = [] }
+      end
+
       def show_additional_data(type)
         res = throb { api.get "#{pull_uri}/#{type}" }.body
         page do
@@ -40,8 +87,12 @@ module GHI
         end
       end
 
+      def web_uri(type)
+        "pull/#{issue}.#{type}"
+      end
+
       def output_from_html(type)
-        res = throb { get_html "pull/#{issue}.#{type}" }
+        res = throb { get_html web_uri(type)}
         page do
           # use the original $stdout.puts, as puts is monkey patched
           # to highlight usernames - not cool when you display code only
