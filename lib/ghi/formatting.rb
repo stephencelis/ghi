a820ff987e406d98eca1a9fd6fb0dea7fd8213b2
rhysd <lin90162@yahoo.co.jp>
2014-10-12 14:28:38 +0900

    Add missing colon when --no-color is specified

--- lib/ghi/formatting.rb
+++ lib/ghi/formatting.rb
@@ -198,7 +198,7 @@ module GHI
     end
 
     def format_number n
-      colorize? ? "#{bright { n }}:" : "#{n} "
+      colorize? ? "#{bright { n }}:" : "#{n}:"
     end
 
     # TODO: Show milestone, number of comments, pull request attached.
