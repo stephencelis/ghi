7583051248ddfd67a1a65a3cdfe06cecf52332e0
Alex Chesters <alex.chesters@bbc.co.uk>
2015-10-17 01:27:32 +0100

    Fixing tab issue

--- lib/ghi/commands/status.rb
+++ lib/ghi/commands/status.rb
@@ -2,31 +2,31 @@ module GHI
   module Commands
     class Status < Command
 
-			def options
+      def options
         OptionParser.new do |opts|
           opts.banner = 'usage: ghi status'
         end
       end
 
-			def execute
-				begin
+      def execute
+        begin
           options.parse! args
           @repo ||= ARGV[0] if ARGV.one?
         rescue OptionParser::InvalidOption => e
           fallback.parse! e.args
           retry
         end
-				
-				require_repo
-				res = throb { api.get "/repos/#{repo}" }.body
+        
+        require_repo
+        res = throb { api.get "/repos/#{repo}" }.body
 
-				if res['has_issues']
-					puts "Issues are enabled for this repo"
-				else
-					puts "Issues are not enabled for this repo"
-				end
+        if res['has_issues']
+          puts "Issues are enabled for this repo"
+        else
+          puts "Issues are not enabled for this repo"
+        end
 
-			end
-		end
-	end
+      end
+    end
+  end
 end
