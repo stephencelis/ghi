e2ba2fac34656022b58ab91852843dd35cb9fa31
Alex Chesters <alex.chesters@bbc.co.uk>
2015-10-17 02:16:58 +0100

    Bump version

--- lib/ghi/commands/version.rb
+++ lib/ghi/commands/version.rb
@@ -1,9 +1,9 @@
 module GHI
   module Commands
     module Version
-      MAJOR   = 0
-      MINOR   = 9
-      PATCH   = 4
+      MAJOR   = 1
+      MINOR   = 0
+      PATCH   = 0
       PRE     = nil
 
       VERSION = [MAJOR, MINOR, PATCH, PRE].compact.join '.'
